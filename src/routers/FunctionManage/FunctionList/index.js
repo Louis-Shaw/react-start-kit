760108395966a0a562b578004e9509491e68cc40
Shaw <506180545@qq.com>
2018-06-26 11:50:21 +0800

    first commit

---
+++ src/routers/FunctionManage/FunctionList/index.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import Loading from  '../../../components/Loading';
+
+import Login from '../../Login'
+class FunctionList extends Component {
+    render() {
+        return (
+            <div className='FunctionList_'>
+                FunctionList
+                <Login/>
+            </div>
+        )
+    }
+}
+
+export default FunctionList
---
+++ src/routers/Home/index.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+import { Cascader ,MonthPicker, RangePicker, WeekPicker, Transfer } from 'antd';
+ 
+class Home extends Component {
+    render() {
+        return (
+            <div className='Home_'>
+            	<Cascader />
+            	<Transfer/>
+
+               home
+            </div>
+        )
+    }
+}
+
+export default Home
