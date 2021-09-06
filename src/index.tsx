526dfbc899f694361f27e221a40992ac461d0313
shaw@kali <506180545@qq.com>
2021-09-07 00:50:18 +0800

    react router

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,14 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
-function Btest() {
-  return (
-    <div>1231231</div>
-  )
-}
+import AppRouter from './routers';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          <Btest />
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <title>test</title>
       </header>
+      <AppRouter/>
     </div>
   );
 }
